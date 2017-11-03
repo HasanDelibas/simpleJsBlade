e513c6b5b7f7d1bed62cb009b67578239c6efecf
Hasan Delibaş <hasan.delibas94@gmail.com>
2017-11-03 22:26:21 +0300

    Create bladeComplier.php

---
+++ bladeComplier.php
@@ -0,0 +1,36 @@
+<pre><?php
+class bladeComplier{
+
+    public static function complie($bladeLocation,$outLocation,$name="Name"){
+
+        $blade = file_get_contents($bladeLocation);
+
+        $from =[
+            "/\{\{\s*@foreach\(\s*([A-Za-z0-9\.\[\]\"\']+)\s* as \s*([A-Za-z0-9\.\[\]\"\']+)\s*\)\s*\}\}/",
+            "/\{\{[^\}]*\@begin\s*\}\}/",
+            "/\{\{\s*\@end\s*\}\}/",
+            "/\{\{([^\}]+)\}\}/"
+        ];
+
+        $to = [
+            "`;\n for(var i=0,$2=$1[Object.keys($1)[i]]; i < Object.keys($1).length ; i++ )",
+            " { retVal+=`",
+            " `; } retVal +=`",
+            "`;\n retVal += $1 ;\n retVal +=`"
+            ];
+            
+        
+
+        $newBlade = preg_replace($from,$to,$blade);
+
+        //$newBlade = str_replace(["&lt;","&gt;"],["<",">"], $newBlade);
+        $newBlade = "$name = function(data){\nvar retVal =`".$newBlade."`;\nreturn retVal;}";
+        print_r(str_replace(["<",">"],["&lt;","&gt;"], $newBlade));
+        file_put_contents( $outLocation , $newBlade );
+
+    }
+
+    
+}
+
+?>
