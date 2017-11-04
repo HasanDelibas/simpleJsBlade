80457e2e06a01ce179b424734d708f89c737c6a3
Hasan Delibaş <hasan.delibas94@gmail.com>
2017-11-05 01:27:55 +0300

    Update bladeComplier.php
    Adding {{ @script }} and {{ @scriptend }}

--- bladeComplier.php
+++ bladeComplier.php
@@ -5,27 +5,53 @@ class bladeComplier{
 
         $blade = file_get_contents($bladeLocation);
 
+        $script= "";
+        
+        $blade = preg_replace("/\n|\t| +/"," ",$blade);
+        
+        $scriptCount = preg_match_all("/\{\{\s*\@script\s*\}\}/",$blade);
+        if( $scriptCount == 1 ){
+            preg_match("/\{\{\s*\@script\s*\}\}(?<scripts>.*)\{\{\s*\@scriptend\s*\}\}/",$blade,$script);
+            $script = $script['scripts'];
+            $blade = preg_replace("/\{\{\s*\@script\s*\}\}(.*)\{\{\s*\@scriptend\s*\}\}/","",$blade);
+            
+        }elseif( $scriptCount==0 ){
+            
+            
+        }else{
+            echo "@script tag count can't more one.";
+            return;
+        }
+        
         $from =[
             "/\{\{\s*@foreach\(\s*([A-Za-z0-9\.\[\]\"\']+)\s* as \s*([A-Za-z0-9\.\[\]\"\']+)\s*\)\s*\}\}/",
-            "/\{\{[^\}]*\@begin\s*\}\}/",
+            "/\{\{\\s*@if\(([^\}]+)\)\s*\}\}/",
+            "/\{\{\s*\@else\s*\}\}/",
+            "/\{\{\s*\@begin\s*\}\}/",
             "/\{\{\s*\@end\s*\}\}/",
             "/\{\{([^\}]+)\}\}/"
         ];
 
         $to = [
-            "`;\n for(var i=0,$2=$1[Object.keys($1)[i]]; i < Object.keys($1).length ; i++ )",
-            " { retVal+=`",
-            " `; } retVal +=`",
-            "`;\n retVal += $1 ;\n retVal +=`"
+            "`;for(var i=0,$2=$1[Object.keys($1)[i]]; i < Object.keys($1).length ; i++ )",
+            "`;if ( $1 ){retVal+=`",
+            "`;}else{retVal+=`",
+            " {retVal+=`",
+            " `;}retVal +=`",
+            "`;retVal+=$1;retVal +=`"
             ];
             
         
 
         $newBlade = preg_replace($from,$to,$blade);
-        $newBlade = "$name = function(data){\nvar retVal =`".$newBlade."`;\nreturn retVal;}";
-        print_r(str_replace(["<",">"],["&lt;","&gt;"], $newBlade));
-        file_put_contents( $outLocation , $newBlade );
+        
+        //$newBlade = preg_replace("/\n/","",$newBlade);
 
+        //$newBlade = str_replace(["&lt;","&gt;"],["<",">"], $newBlade);
+        $newBlade = "$name=function(data){var retVal=`".$newBlade."`;return retVal;};".$script.";";
+        
+        file_put_contents( $outLocation , $newBlade );
+        return $newBlade;
     }
 
     
